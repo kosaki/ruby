9047b3d48c13bf06afaad1ecd04b9b5d8357be37
KOSAKI Motohiro <kosaki.motohiro@gmail.com>
2015-12-15 02:39:04 -0500

    * lib/monitor.rb (MonitorMixin::ConditionVariable#wait): use Thread.handle_interrupt for avoiding races.

--- lib/monitor.rb
+++ lib/monitor.rb
@@ -105,13 +105,15 @@ module MonitorMixin
     # even if no other thread doesn't signal.
     #
     def wait(timeout = nil)
-      @monitor.__send__(:mon_check_owner)
-      count = @monitor.__send__(:mon_exit_for_cond)
-      begin
-        @cond.wait(@monitor.instance_variable_get(:@mon_mutex), timeout)
-        return true
-      ensure
-        @monitor.__send__(:mon_enter_for_cond, count)
+      Thread.handle_interrupt(Exception => :on_blocking) do
+        @monitor.__send__(:mon_check_owner)
+        count = @monitor.__send__(:mon_exit_for_cond)
+        begin
+          @cond.wait(@monitor.instance_variable_get(:@mon_mutex), timeout)
+          return true
+        ensure
+          @monitor.__send__(:mon_enter_for_cond, count)
+        end
       end
     end
 
